93665b571f28cfe7f8870f2ff9cd8d45532c22ee
Surya Kumara <suryakumara33@gmail.com>
2021-07-04 12:09:32 +0800

    feat: add list item and side bar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,91 @@
 import { makeStyles } from "@material-ui/core";
 import React from "react";
+import { Drawer } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { List } from "@material-ui/core";
+import { ListItem } from "@material-ui/core";
+import { ListItemIcon } from "@material-ui/core";
+import { ListItemText } from "@material-ui/core";
+import { AddCircleOutlined, SubjectOutlined } from "@material-ui/icons";
+import { useHistory, useLocation } from "react-router";
 
-const useStyles = makeStyles({
-  page: {
-    background: "#f9f9f9",
-    width: "100%",
-  },
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => {
+  return {
+    page: {
+      background: "#f9f9f9",
+      width: "100%",
+      padding: theme.spacing(3),
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    root: {
+      display: "flex",
+    },
+    active: {
+      background: "#f4f4f4",
+    },
+    title: {
+      padding: theme.spacing(2),
+    },
+  };
 });
 
 export default function Layout({ children }) {
   const classes = useStyles();
+  const history = useHistory();
+  const location = useLocation();
+
+  const menuItems = [
+    {
+      text: "My Notes",
+      icon: <SubjectOutlined color="secondary" />,
+      path: "/",
+    },
+    {
+      text: "Create Notes",
+      icon: <AddCircleOutlined color="secondary" />,
+      path: "/create",
+    },
+  ];
+
   return (
-    <div>
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography className={classes.title} variant="h5">
+            NCIT Notes
+          </Typography>
+        </div>
+
+        {/* list/link */}
+
+        <List>
+          {menuItems.map((menu) => (
+            <ListItem
+              key={menu.text}
+              button
+              onClick={() => history.push(menu.path)}
+              className={
+                location.pathname === menu.path ? classes.active : null
+              }
+            >
+              <ListItemIcon>{menu.icon}</ListItemIcon>
+              <ListItemText primary={menu.text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
       <div className={classes.page}>{children}</div>
     </div>
   );
--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { Card, IconButton } from "@material-ui/core";
+import { Card, IconButton, makeStyles } from "@material-ui/core";
 import { CardHeader } from "@material-ui/core";
 import { CardContent } from "@material-ui/core";
 import { DeleteOutlined } from "@material-ui/icons";
 import { Typography } from "@material-ui/core";
 
+const useStyles = makeStyles({
+  test: {
+    border: (note) => {
+      if (note.category === "work") {
+        return "1px solid red";
+      }
+    },
+  },
+});
+
 export default function NoteCard({ note, handleDelete }) {
+  const classes = useStyles(note);
   return (
     <div>
-      <Card elevation={1}>
+      <Card elevation={1} className={classes.test}>
         <CardHeader
           action={
             <IconButton onClick={() => handleDelete(note.id)}>
